dd078dcaee1e3a01b65debfbdb9b5f735627304e
Asif-vai <mr3366533@gmail.com>
2026-01-13 18:55:24 +0600

    export user model

--- models/user.model.js
+++ models/user.model.js
@@ -26,6 +26,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = User;
